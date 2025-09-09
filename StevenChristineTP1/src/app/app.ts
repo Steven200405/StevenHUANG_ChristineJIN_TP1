788b98a14721b03f24b7d6fcf85251ceb0e2bd86
Steven200405 <huangsteven2004@gmail.com>
2025-09-09 21:07:25 +0200

    Version 3 non terminée

--- StevenChristineTP1/src/app/app.ts
+++ StevenChristineTP1/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import {Component, Output, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { Footer } from "./footer/footer";
@@ -14,4 +14,7 @@ export class App {
   protected readonly title = signal('StevenChristineTP1');
 
   public headerColor: string = "";
+  public changeColor(){
+    this.colorRed="blue";
+  }
 }
--- StevenChristineTP1/src/app/center/center.ts
+++ StevenChristineTP1/src/app/center/center.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-center',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './center.scss'
 })
 export class Center {
-
+  @Output() color = new EventEmitter();
 }
