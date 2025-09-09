361d3e999ccf4598fb75f1b8d5bdde122de477f4
JinChristine <jin.christine_0111@hotmail.com>
2025-09-09 16:23:16 +0200

    Version 1

---
+++ StevenChristineTP1/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
+import { Center } from './center/center';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Footer, Center],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('StevenChristineTP1');
+
+  public headerColor: string = "";
+}
---
+++ StevenChristineTP1/src/app/center/center.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-center',
+  imports: [],
+  templateUrl: './center.html',
+  styleUrl: './center.scss'
+})
+export class Center {
+
+}
